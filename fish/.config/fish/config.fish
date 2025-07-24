7051869f88fea4fa58d812b62e8a0dce8a2a5249
div <rasaniadivya1@gmail.com>
2025-07-24 10:24:58 -0400

    organized fish config into section

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,21 @@
 if status is-interactive
     set fish_greeting
 
+    # ---EXPORTS---
     export EDITOR='nvim'
     export VISUAL=$EDITOR
 
+    # ---INSTALLERS---
+    # vencord: `sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"`
+    # catppuccin-kde: `git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde`
+
+    # ---ALIAS---
     alias ll='ls -alF'
     alias la='ls -A'
     alias l='ls -CF'
     alias edit-rc='$EDITOR ~/.config/fish/config.fish; source ~/.config/fish/config.fish'
-    alias install-vencord='sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"'
 end
 
+# ---RANDOMS??---
 /home/div/.local/bin/mise activate fish | source
 zoxide init --cmd cd fish | source
