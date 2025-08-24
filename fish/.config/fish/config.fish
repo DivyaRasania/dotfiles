4e363656e060281ef029e1a5b2f133671c0dd6e8
div <rasaniadivya1@gmail.com>
2025-08-23 23:35:01 -0400

    updated fish config with some useful aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,23 @@
-if status is-interactive
-    set fish_greeting
+# ---RANDOMS??---
+set fish_greeting
+fish_add_path ~/.local/bin
+mise activate fish | source
+zoxide init --cmd cd fish | source
 
+if status is-interactive
     # ---EXPORTS---
     export EDITOR='nvim'
     export VISUAL=$EDITOR
 
     # ---ALIAS---
-    alias ll='ls -alF'
-    alias la='ls -A'
-    alias l='ls -CF'
+    alias l='eza --color=always --group-directories-first --icons' # preferred listing
+    alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
+    alias ll='eza -la --color=always --group-directories-first --icons' # long format
+    alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+    alias l.="eza -a | grep -e '^\.'" # show only dotfiles
+    alias cat='bat --theme zenburn'
+    alias tarnow='tar -acf '
+    alias untar='tar -zxvf '
+    alias grep='grep --color=auto'
     alias edit-rc='$EDITOR ~/.config/fish/config.fish; source ~/.config/fish/config.fish'
 end
-
-# ---RANDOMS??---
-mise activate fish | source
-zoxide init --cmd cd fish | source
