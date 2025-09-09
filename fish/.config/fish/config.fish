b060f5acab83f343f3cf4b0e10e4a9103fc683e9
div <rasaniadivya1@gmail.com>
2025-09-08 22:38:42 -0400

    removed end-4 fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,4 @@
 # ---RANDOMS??---
-# ---ENDS-4-HYPRLAND-CONFIG---
-function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
-    printf '%s@%s %s%s%s > ' $USER $hostname (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-end
-
 set fish_greeting
 
 fish_add_path ~/.local/bin
@@ -20,13 +12,6 @@ if status is-interactive
     # ---EXPORTS---
     set -Ux VISUAL nvim
     set -Ux EDITOR "$VISUAL"
-
-    # ---ENDS-4-HYPRLAND-CONFIG---
-    # Use starship
-    starship init fish | source
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    end
 
     # ---ALIAS---
     abbr -a ls "eza --color=always --group-directories-first --icons" # preferred listing
