efd30b8c3c06067e1fc1d1655f95a9384e1ce87c
div <rasaniadivya1@gmail.com>
2026-06-19 20:24:27 +0000

    format changes and added tmux aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,12 +15,12 @@ if status is-interactive
     set -Ux EDITOR "$VISUAL"
 
     # ---ALIAS---
-    alias ls="eza --color=always --group-directories-first --icons" # preferred listing
-    alias l="eza --color=always --group-directories-first --icons" # preferred listing
-    alias la="eza -a --color=always --group-directories-first --icons" # all files and dirs
+    alias ls="eza --color=always --group-directories-first --icons"     # preferred listing
+    alias l="eza --color=always --group-directories-first --icons"      # preferred listing
+    alias la="eza -a --color=always --group-directories-first --icons"  # all files and dirs
     alias ll="eza -la --color=always --group-directories-first --icons" # long format
     alias lt="eza -aT --color=always --group-directories-first --icons" # tree listing
-    alias l.="eza -a | rg -e '^\.'" # show only dotfiles
+    alias l.="eza -a | rg -e '^\.'"                                     # show only dotfiles
     
     if command -q apt
         alias cat="batcat"
@@ -33,4 +33,8 @@ if status is-interactive
     alias grep="rg --color=auto"
     
     alias edit-rc="$EDITOR ~/.config/fish/config.fish; source ~/.config/fish/config.fish"
+    alias reload-rc="source ~/.config/fish/config.fish"
+
+    alias edit-tmux="$EDITOR ~/.tmux.conf; tmux source ~/.tmux.conf"
+    alias reload-tmux="tmux source ~/.tmux.conf"
 end
